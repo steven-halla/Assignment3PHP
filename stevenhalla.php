bdcedad9ad1ab4d88fd73cc0988fac30ab98a341
steven-halla <steven.r.halla@gmail.com>
2024-05-04 15:52:29 -0700

    first-this has correct way to do text file which I need for assignemnt 2

---
+++ stevenhalla.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Page</title>
+    <style>
+        body {
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+<h1>My second PHP page in Amazon WorkSpaces Client</h1>
+<h1>Steven Halla!</h1>
+</body>
+</html>
